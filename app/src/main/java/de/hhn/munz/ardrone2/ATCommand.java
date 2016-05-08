c3b6364b36acc066bc315dae50403e4d33d0780a
Manuel Munz <manumunz@web.de>
2016-05-08 20:00:33 +0200

    +ATCommands and Networking

---
+++ app/src/main/java/de/hhn/munz/ardrone2/ATCommand.java
@@ -0,0 +1,67 @@
+package de.hhn.munz.ardrone2;
+
+public class ATCommand {
+    public static String keepAlive() {
+        return "AT*COMWDG=%d\r";
+    }
+
+    public static String hover() {
+        return "AT*PCMD=%d,1,0,0,0,0\r";
+    }
+
+    public static String land() {
+        return "AT*REF=%d,290717696\r";
+    }
+
+    public static String takeOff() {
+        return "AT*REF=%d,290718208\r";
+    }
+
+    private static String move(float pitch, float roll, float gaz, float yaw) {
+        return "AT*PCMD=%d,1,"
+                + Float.floatToIntBits(pitch) + ","
+                + Float.floatToIntBits(roll) + ","
+                + Float.floatToIntBits(gaz) + ","
+                + Float.floatToIntBits(yaw) + "\r";
+    }
+
+    public static String forward(float speed) {
+        return "AT*PCMD=%d,1,0," + Float.floatToIntBits(-speed) + ",0,0\r";
+    }
+
+    public static String backward(float speed) {
+        return "AT*PCMD=%d,1,0," + Float.floatToIntBits(speed) + ",0,0\r";
+    }
+
+    public static String left(float speed) {
+        return "AT*PCMD=%d,1," + Float.floatToIntBits(-speed) + ",0,0,0\r";
+    }
+
+    public static String right(float speed) {
+        return "AT*PCMD=%d,1," + Float.floatToIntBits(speed) + ",0,0,0\r";
+    }
+
+    public static String up(float speed) {
+        return "AT*PCMD=%d,1,0,0," + Float.floatToIntBits(speed) + ",0\r";
+    }
+
+    public static String down(float speed) {
+        return "AT*PCMD=%d,1,0,0," + Float.floatToIntBits(-speed) + ",0\r";
+    }
+
+    public static String rotateLeft(float speed) {
+        return "AT*PCMD=%d,1,0,0,0," + Float.floatToIntBits(-speed) + "\r";
+    }
+
+    public static String rotateRight(float speed) {
+        return "AT*PCMD=%d,1,0,0,0," + Float.floatToIntBits(speed) + "\r";
+    }
+
+    public static String maxAltitude(int millimeter) {
+        return "AT*CONFIG=%d,\"control:altitude_max\",\""+millimeter+"\"\r";
+    }
+
+    public static String emergencyReset() {
+        return "AT*REF=%d,290717952\r";
+    }
+}
---
+++ app/src/main/java/de/hhn/munz/ardrone2/NetworkController.java
@@ -0,0 +1,86 @@
+package de.hhn.munz.ardrone2;
+
+import android.util.Log;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class NetworkController {
+    private static final String TAG = ".NetworkController";
+
+    private static final String SERVER_HOST = "192.168.1.1";
+    private static final int SERVER_PORT = 5556;
+
+    private static NetworkController instance;
+
+    private DatagramSocket socket;
+    private int sequenceNumber;
+    private boolean keepAlive;
+
+    private NetworkController () {
+        try {
+            socket = new DatagramSocket();
+            sequenceNumber = 0;
+            keepAlive = true;
+            this.keepAlive();
+        } catch (Exception e) {
+            Log.w(TAG, e.getMessage());
+        }
+    }
+
+    public static NetworkController getInstance() {
+        if (NetworkController.instance == null) {
+            NetworkController.instance = new NetworkController();
+        }
+        return NetworkController.instance;
+    }
+
+    public synchronized void sendString(String data) {
+        try {
+            InetAddress IPAddress =  InetAddress.getByName(SERVER_HOST);
+
+            byte[] bytes = String.format(data,++sequenceNumber).getBytes();
+
+            DatagramPacket packet = new DatagramPacket(bytes,bytes.length, IPAddress, SERVER_PORT);
+            socket.send(packet);
+        } catch (Exception e) {
+            Log.w(TAG, e.getMessage());
+        }
+    }
+
+    private void keepAlive() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (keepAlive) {
+                    try {
+                        sendString(ATCommand.keepAlive());
+                        Thread.sleep(200);
+                    } catch (Exception e) {
+                        Log.w(TAG, "KeepAlive: " + e.getMessage());
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public void closeConnection() {
+        keepAlive = false;
+        socket.close();
+        socket = null;
+    }
+
+    public void restartConnection() {
+        try {
+            if (!keepAlive) {
+                socket = new DatagramSocket();
+                sequenceNumber = 0;
+                keepAlive = true;
+                this.keepAlive();
+            }
+        } catch (Exception e) {
+            Log.w(TAG, e.getMessage());
+        }
+    }
+}
